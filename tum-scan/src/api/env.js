831c44f4f9dc1de5907acbc9a1e576a125a3f995
zhongfonan <zhongfonan@egova.com.cn>
2018-06-06 21:09:12 +0800

    解决prettier高于1.13版本时vue-loader错误问题，以及环境变量baseURL配置问题

--- tum-scan/src/api/env.js
+++ tum-scan/src/api/env.js
@@ -7,6 +7,9 @@
  \*/
 export default {
   // baseURL: 'http://state.jingtum.com',
-  baseURL: process.env.API_BASE,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8081/api/v1"
+      : "http://106.14.65.102:8081/api/v1",
   isDev: true
-}
+};
